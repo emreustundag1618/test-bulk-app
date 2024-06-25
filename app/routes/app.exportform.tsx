1c22bc8e8c76fea9361615be9f456caffb8a1990
emreustundag1618 <emre.ustundag.1.618@gmail.com>
2024-06-25 22:58:19 +0300

    Last updates

--- app/routes/app.exportform.tsx
+++ app/routes/app.exportform.tsx
@@ -1,31 +1,141 @@
-import { ActionFunction } from "@remix-run/node";
-import { Page} from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
+import { ActionFunction, redirect } from "@remix-run/node";
+import { useActionData, useSubmit } from "@remix-run/react";
+import {
+  Button,
+  Card,
+  Layout,
+  Page,
+  Popover,
+  ResourceListProps,
+  Text,
+} from "@shopify/polaris";
+import { useCallback, useState } from "react";
+import {
+  ResourceListExample,
+  itemsResource,
+} from "~/components/CustomResourceList";
+import { productsQuery } from "~/graphql/productsQuery";
+import { authenticate } from "~/shopify.server";
 
-export interface IExportFormProps {
-}
+type Props = {};
 
-export const action: ActionFunction = async({request}) => {
-    return null;
-}
+export const action: ActionFunction = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const formData = await request.formData();
 
-export default function ExportForm (props: IExportFormProps) {
+  const response = await admin.graphql(`
+  #graphql
+    mutation {
+        bulkOperationRunQuery(
+    query: """
+     ${productsQuery}
+    """
+    ) {
+    bulkOperation {
+      id
+      status
+      query
+      rootObjectCount
+      type
+      partialDataUrl
+      objectCount
+      fileSize
+      createdAt
+      url
+    }
+    userErrors {
+      field
+      message
+    }
+  }
+} 
+`);
 
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data.data.bulkOperationRunQuery.bulkOperation, "data");
+
+    //pass values function
+
+    return redirect("/app/exportresult");
+  }
+
+  return null;
+};
+
+const ExportForm = (props: Props) => {
+  const [activate, setActivate] = useState(false);
+
+  const [selectedItems, setSelectedItems] = useState<
+    ResourceListProps["selectedItems"]
+  >([]);
+
+  console.log(selectedItems, "selectedItems");
+
+  const toggleActive = useCallback(
+    () => setActivate((activate) => !activate),
+    [],
+  );
+
+  const submit = useSubmit();
+  const actiondata = useActionData<typeof action>();
+  console.log(actiondata, "actiondatahook");
+
+  const activator = (
+    <Button onClick={toggleActive} disclosure>
+      select sheets
+    </Button>
+  );
+
+  const createExport = () => {
+    submit(
+      {},
+      {
+        replace: true,
+        method: "POST",
+        action: "/app/exportform",
+      },
+    );
+  };
 
   return (
     <Page>
-      <TitleBar title="Export form">
-        <button variant="breadcrumb">
-            Home
+      <ui-title-bar title="New Export">
+        <button variant="breadcrumb">Home</button>
+        <button onClick={() => {}}>Back</button>
+        <button variant="primary" onClick={createExport}>
+          Export
         </button>
-        <button onClick={() => {}}>
-            Back
-        </button>
-        <button variant="primary" onClick={() => {}}>
-            Export
-        </button>
-      </TitleBar>
+      </ui-title-bar>
+      <Layout>
+        <Layout.Section>
+          <Card>
+            <Text as="p" fontWeight="bold">
+              Format: CSV
+            </Text>
+          </Card>
+          <br />
+          <Card>
+            <div style={{ position: "relative" }}>
+              <Popover
+                activator={activator}
+                onClose={toggleActive}
+                fullWidth
+                autofocusTarget="first-node"
+                active={activate}
+              >
+                <ResourceListExample
+                  items={itemsResource}
+                  selectedItems={selectedItems}
+                  setSelectedItems={setSelectedItems}
+                />
+              </Popover>
+            </div>
+          </Card>
+        </Layout.Section>
+      </Layout>
     </Page>
   );
-}
+};
 
+export default ExportForm;
